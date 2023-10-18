f51e3c1e1813c137255df83d94f1f5b1b202295d
doanxuantuan <92573433+doanxuantuan@users.noreply.github.com>
2023-10-19 00:45:52 +0700

    admn
    quản lý danh mục, loại sp

---
+++ WebSieuThiMiNi/WebsiteBanHang/WebsiteBanHang/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Models;
+
+namespace WebsiteBanHang.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public AccountController()
+        {
+
+        }
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+        // GET: Admin/Account
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        public ActionResult Index()
+        {
+            var ítems = db.Users.ToList();
+            return View(ítems);
+        }
+    }
+}
