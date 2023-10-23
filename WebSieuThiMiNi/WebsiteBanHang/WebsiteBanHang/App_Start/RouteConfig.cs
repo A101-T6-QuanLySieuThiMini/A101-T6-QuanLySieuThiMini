edddeb284678ffb963603592bf7c854e8c17b7a6
doanxuantuan <92573433+doanxuantuan@users.noreply.github.com>
2023-10-23 22:47:30 +0700

    product

--- WebSieuThiMiNi/WebsiteBanHang/WebsiteBanHang/App_Start/RouteConfig.cs
+++ WebSieuThiMiNi/WebsiteBanHang/WebsiteBanHang/App_Start/RouteConfig.cs
@@ -12,7 +12,26 @@ namespace WebsiteBanHang
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+             name: "Contact",
+             url: "lien-he",
+             defaults: new { controller = "Contact", action = "Index", alias = UrlParameter.Optional },
+             namespaces: new[] { "WebsiteBanHang.Controllers" }
+         );
 
+            routes.MapRoute(
+             name: "CategoryProduct",
+             url: "danh-muc-san-pham/{alias}-{id}",
+             defaults: new { controller = "Products", action = "ProductCategory", alias = UrlParameter.Optional },
+             namespaces: new[] { "WebsiteBanHang.Controllers" }
+         );
+            routes.MapRoute(
+             name: "Products",
+             url: "san-pham",
+             defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional },
+             namespaces: new[] { "WebsiteBanHang.Controllers" }
+         );
+           
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- WebSieuThiMiNi/WebsiteBanHang/WebsiteBanHang/Controllers/ProductsController.cs
+++ WebSieuThiMiNi/WebsiteBanHang/WebsiteBanHang/Controllers/ProductsController.cs
@@ -11,12 +11,26 @@ namespace WebsiteBanHang.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(int? id)
         {
             var items = db.Products.ToList();
+            if (id!=null)
+            {
+                items = items.Where(x => x.ProductCategoryId == id).ToList();
+            }    
             return View(items);
         }
-        public ActionResult Partial_ItemByCateId()
+        public ActionResult ProductCategory(string alias,int id)
+        {
+            var items = db.Products.ToList();
+            if (id >0)
+            {
+                items = items.Where(x => x.ProductCategoryId == id).ToList();
+            }
+            ViewBag.CateId = id;
+            return View(items);
+        }
+        public ActionResult Partial_ItemByCateId(string alias )
         {
             var items = db.Products.Where(x => x.IsHome).ToList();
             return PartialView();
