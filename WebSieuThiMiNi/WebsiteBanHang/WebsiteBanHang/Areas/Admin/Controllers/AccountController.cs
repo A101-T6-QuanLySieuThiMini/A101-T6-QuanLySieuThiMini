f338a175227a39d82100d1112de1e3d9e6addfd0
doanxuantuan <92573433+doanxuantuan@users.noreply.github.com>
2023-11-26 23:43:01 +0700

    account
    k đăng nhập được

--- WebSieuThiMiNi/WebsiteBanHang/WebsiteBanHang/Areas/Admin/Controllers/AccountController.cs
+++ WebSieuThiMiNi/WebsiteBanHang/WebsiteBanHang/Areas/Admin/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Microsoft.Owin.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using WebsiteBanHang.Models;
@@ -17,14 +18,13 @@ namespace WebsiteBanHang.Areas.Admin.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
         public AccountController()
         {
-
         }
+
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
         }
-        // GET: Admin/Account
 
         public ApplicationSignInManager SignInManager
         {
@@ -49,10 +49,115 @@ namespace WebsiteBanHang.Areas.Admin.Controllers
                 _userManager = value;
             }
         }
+        //
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return RedirectToLocal(returnUrl);
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                    return View(model);
+            }
+        }
+
+        // GET: Admin/Account
         public ActionResult Index()
         {
-            var ítems = db.Users.ToList();
-            return View(ítems);
+            var items = db.Users.ToList(); ;
+            return View(items);
+        }
+        [AllowAnonymous]
+        public ActionResult Create()
+        {
+            ViewBag.Role = new SelectList(db.Roles.ToList(), "Id", "Name");
+            return View();    
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateAccountViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    FullName = model.FullName,
+                    Phone = model.Phone
+                };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    //erManager.AddToRole(user.Id, model.Role);
+                    //await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+            ViewBag.Role = new SelectList(db.Roles.ToList(), "Name", "Name");
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
         }
     }
 }
--- WebSieuThiMiNi/WebsiteBanHang/WebsiteBanHang/Areas/Admin/Controllers/HomeController.cs
+++ WebSieuThiMiNi/WebsiteBanHang/WebsiteBanHang/Areas/Admin/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace WebsiteBanHang.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         // GET: Admin/Home
         public ActionResult Index()
         {
